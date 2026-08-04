58226ca057904aa4cac5be68738b9d5431ca47ca
Rohit Maurya <126585496+rohitmaurya07@users.noreply.github.com>
2026-08-04 13:08:04 +0530

    Update CORS origin to specific URL

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ import companyRoutes from "./src/routes/company.routes.js";
 const app = express()
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://workkforce.up.railway.app",
   credentials: true,
   methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
