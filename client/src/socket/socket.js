f103b79b006546080f43a1f5db1aec929c5245b2
Rohit Maurya <126585496+rohitmaurya07@users.noreply.github.com>
2026-08-04 13:16:56 +0530

    Update socket.js

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_BACKEND_BASE_URL, {
+export const socket = io(import.meta.env.VITE_BACKEND_URL, {
     withCredentials: true,
 });
