b45ae88830642be4eb9dcf5db63b245da97157b8
Rohit Maurya <126585496+rohitmaurya07@users.noreply.github.com>
2026-08-04 12:48:52 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    allowedHosts: [
+      "calm-sparkle-production-3504.up.railway.app"
+    ]
+  }
 })
