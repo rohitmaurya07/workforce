7f08ea2bef0249aa0b2815138d787f02b1c3cffb
Rohit Maurya <126585496+rohitmaurya07@users.noreply.github.com>
2026-08-04 12:37:12 +0530

    Update project.controllers.js

--- server/src/controllers/project.controllers.js
+++ server/src/controllers/project.controllers.js
@@ -1,6 +1,6 @@
 import Project from "../models/Project.Model.js";
 import Task from "../models/Task.Model.js";
-import User from "../models/User.Model.js";
+import User from "../models/User.model.js";
 
 export const createProject = async (
   req,
