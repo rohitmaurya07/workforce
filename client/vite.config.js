7cd2f4a2233f12a8f1f0ee3dfd55a1d5d58ede55
Rohit Maurya <126585496+rohitmaurya07@users.noreply.github.com>
2026-08-04 13:02:43 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: process.env.PORT,
     allowedHosts: [
       "calm-sparkle-production-3504.up.railway.app",
-      "workkforce.up.railway.app
+      "workkforce.up.railway.app",
     ]
   }
 })
